7e7a67b2baa4f48779b059736795d23b401130e8
ACANTELOUP <anthony.canteloup@ynov.com>
2024-02-12 10:45:10 +0100

    clean code and old try

--- script.js
+++ script.js
@@ -4,17 +4,10 @@ import { Code } from "./PassCodeClass.js";
 export function hashDigit(digit) {
     return sha256(digit.toString());
 }
-  
-  // Combinaison de chiffres correcte (1, 2, 3)
-// const correctCombination = {
-//     digit1: hashDigit(1),
-//     digit2: hashDigit(2),
-//     digit3: hashDigit(3)
-// }
- 
-const code = new Code()
 
-  // Fonction pour vérifier une combinaison
+const code = new Code();
+
+// Fonction pour vérifier une combinaison
 function checkCombination(premier = document.getElementById('input1').value,
                           deuxieme = document.getElementById('input2').value,
                           troisieme = document.getElementById('input3').value) {
@@ -28,14 +21,14 @@ function checkCombination(premier = document.getElementById('input1').value,
         hashedDigit3 == code.digit3
     ) {
         console.log('Combinaison correcte.');
-        console.log(premier, deuxieme, troisieme)
+        console.log(premier, deuxieme, troisieme);
         document.getElementById('result').innerText = "Vous avez cassé le code";
         document.getElementById('result').id = "Succes";
         return(true)
     } else {
         console.log('Combinaison incorrecte.');
         document.getElementById('result').innerText = "Combinaison incorrecte";
-        return(false)
+        return(false);
     }
 }
   
